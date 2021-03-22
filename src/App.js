7fa948ab4b80a22814587d1b4c3a2352853ab3cc
Arko Chandra <arkoch7392@gmail.com>
2021-03-22 11:53:08 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import React, { Component } from "react";
 import Register from "./components/Inbox";
 import Login from "./components/Login";
-
+import { BrowserRouter, Route } from "react-router-dom";
+import { Security, SecureRoute } from "@okta/okta-react";
 
 class App extends Component {
   render() {
     return(
+      <BrowserRouter>
+        <Security >
       <div className="App">
         <Login/>
       </div>
+        </Security>
+      </BrowserRouter>
     )
   }
 
